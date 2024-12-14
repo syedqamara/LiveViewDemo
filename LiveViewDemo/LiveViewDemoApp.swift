988d551ea567d8daf093845b23e189c1201c4e55
Qamar Abbas <syedqamar.a1@gmail.com>
2024-12-15 04:56:15 +0500

    Fixes

--- LiveViewDemo/LiveViewDemoApp.swift
+++ LiveViewDemo/LiveViewDemoApp.swift
@@ -21,6 +21,7 @@ struct LiveViewDemoApp: App {
     var body: some Scene {
         WindowGroup {
             NavigationStack {
+                // Displaying a view that load images from bundle see `ViewFactory+Photos.swift`
                 viewFactory.swiftUI(input: .bundle)
                     .environment(\.viewFactory, viewFactory)
                     .environment(\.managedObjectContext, persistenceController.container.viewContext)
@@ -28,9 +29,11 @@ struct LiveViewDemoApp: App {
             .onAppear() {
                 self.appManager.setup()
             }
+            // Binding Global Alert Handler to display alert thrown from any where
             .fullScreenCover(item: $appManager.alert) { content in
                 viewFactory.swiftUI(input: .alert(content, $isAlertDismissed))
             }
+            // Binding Global Loading View to display loading from any where
             .fullScreenCover(isPresented: $appManager.isLoading, content: {
                 viewFactory.swiftUI(input: .loading("Please wait"))
             })
